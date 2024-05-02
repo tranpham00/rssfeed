fac332daa965cf0fc2857dc081251f845ca7cf9b
tran pham <trannpham.05@gmail.com>
2024-05-02 10:55:34 -0500

    dependency and code from assignment

--- src/main/java/edu/francistuttle/App.java
+++ src/main/java/edu/francistuttle/App.java
@@ -1,7 +1,14 @@
 package edu.francistuttle;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
@@ -12,6 +19,25 @@ public class App
 {
     public static void main( String[] args )
     {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        try (CloseableHttpResponse response1 = client.execute(httpGet)) {
+            final HttpEntity entity = response1.getEntity();
+            if (entity != null) {
+                try (InputStream inputStream = entity.getContent()) {
+			    // pass inputStream to builder.parse()
+                    
+
+
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Hosed: " + e.toString());
+        }
+
+
         try
         {
             File path = new File("C:\\Users\\ap1101037\\Desktop\\github-tran\\rssfeed\\src\\nasa.xml");
